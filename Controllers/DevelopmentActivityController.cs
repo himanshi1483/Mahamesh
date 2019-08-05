ad19ba233852208e3a6fe832a6ee334d186186d9
Himanshi Dwivedi <himanshi1483@gmail.com>
2019-08-05 22:59:25 +0530

    upd

---
+++ Controllers/DevelopmentActivityController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mahamesh.Controllers
+{
+    public class DevelopmentActivityController : Controller
+    {
+        // GET: DevelopmentActivity
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DistributionBreeding()
+        {
+            return View();
+        }
+        public ActionResult Distribution()
+        {
+            return View();
+        }
+        public ActionResult Training()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SchemesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mahamesh.Controllers
+{
+    public class SchemesController : Controller
+    {
+        // GET: Schemes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ImplimentedSchemes()
+        {
+            return View();
+        }
+
+        public ActionResult InProgressSchemes()
+        {
+            return View();
+        }
+    }
+}
