9ca65161931448b4481b52cd35d66082ae269e38
Himanshi Dwivedi <himanshi1483@gmail.com>
2019-08-06 23:06:04 +0530

    upd

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mahamesh.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult ProductsAtFarm()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult ProductsWool()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult StockAvailability()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult DeptGuidelines()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult DeptGuidelinesGR()
+        {
+            return View();
+        }
+        // GET: Menu
+        public ActionResult BeneficiaryList()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult ProjectStatus()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult OfficeDocuments()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult AuditedReports()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult Information()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult OfficersList()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult InseminationInformation()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult HospitalsInformation()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult BreedsAvailable()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult VaccinationList()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult FeedingInformation()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult BreedingInformation()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult OngoingSchemes()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult AnimalTradingPolicy()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult ModelScheme()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult FeedandFodder()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult ProductAvailibility()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult SuccessStories()
+        {
+            return View();
+        }
+
+        // GET: Menu
+        public ActionResult FarmersList()
+        {
+            return View();
+        }
+
+
+        // GET: Menu
+        public ActionResult LivestockInsurance()
+        {
+            return View();
+        }
+
+
+        // GET: Menu
+        public ActionResult Precautions()
+        {
+            return View();
+        }
+
+
+        // GET: Menu
+        public ActionResult WebsitePolicy()
+        {
+            return View();
+        }
+    }
+}
