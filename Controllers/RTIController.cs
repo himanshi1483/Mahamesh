fb4a2fb9f6a9715362462aa7152b4217dc8145fb
Himanshi Dwivedi <himanshi1483@gmail.com>
2019-08-06 13:45:55 +0530

    changes

---
+++ Controllers/RTIController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mahamesh.Controllers
+{
+    public class RTIController : Controller
+    {
+        // GET: RTI
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult RTIStatus()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult RTIDisposal()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult Statistics()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult ProactiveDisclosure()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult OnlineRTIApplication()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult OnlineStatus()
+        {
+            return View();
+        }
+
+        // GET: RTI
+        public ActionResult PromotionStatus()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UsefulLinksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mahamesh.Controllers
+{
+    public class UsefulLinksController : Controller
+    {
+        // GET: UsefulLinks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsefulLinks
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
