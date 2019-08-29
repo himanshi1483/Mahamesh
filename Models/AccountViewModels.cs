4ff08c7a60b533f7a79d1983f7182827c1aff41a
Himanshi Dwivedi <himanshi1483@gmail.com>
2019-08-29 20:24:10 +0530

    ups

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -48,10 +48,15 @@ namespace Mahamesh.Models
 
     public class LoginViewModel
     {
+        //[Required]
+        //[Display(Name = "Email")]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        //[EmailAddress]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
