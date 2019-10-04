15ac04cb7c956e8901810968fe355f57fb06bc87
Himanshi Dwivedi <himanshi1483@gmail.com>
2019-10-04 13:11:19 +0530

    upd

--- App_LocalResources/Lang.Designer.cs
+++ App_LocalResources/Lang.Designer.cs
@@ -19,7 +19,7 @@ namespace Mahamesh.App_LocalResources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Lang {
@@ -1038,6 +1038,15 @@ namespace Mahamesh.App_LocalResources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Expo.
+        /// </summary>
+        public static string Expo {
+            get {
+                return ResourceManager.GetString("Expo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Extension Activity.
         /// </summary>
         public static string ExtensionActivity {
@@ -1151,6 +1160,15 @@ namespace Mahamesh.App_LocalResources {
         public static string FarmManager {
             get {
                 return ResourceManager.GetString("FarmManager", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Farms.
+        /// </summary>
+        public static string Farms {
+            get {
+                return ResourceManager.GetString("Farms", resourceCulture);
             }
         }
         
